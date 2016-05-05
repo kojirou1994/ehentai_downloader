4826944d9f9dbcdf39549e1d6b75ac8375bb231a
kojirou <kojirouhtc@gmail.com>
2016-05-05 18:09:39 +0800

    retry

--- main.py
+++ main.py
@@ -14,6 +14,54 @@ def find_title(first_page):
         os.mkdir(title)
         return title
 
+def download_img(url, path, index):
+    fails = 0
+    while True:
+        try:
+            if fails > 3:
+                print "Page %d Download Fail." % index
+                print "Image Url: " + url
+                break
+            print "Downloading Page %d" % index
+            file = urllib2.urlopen(url)
+        except:
+            fails += 1
+            print "Downloading Page %d Fail %d" % (index, fails)
+        else:
+            with open(path, "wb") as code:
+                code.write(file.read())
+            print "Downloaded Page %d" % index
+            break
+
+def open_page(url, title):
+    index = 0
+    last_page = None
+    current_page = url
+    while True:
+        index += 1
+        if (last_page == current_page):
+            break
+        else:
+            page_fails = 0
+            while True:
+                try:
+                    if page_fails > 3:
+                        print 'Page %d Open Failed' % index
+                        print 'Download Failed'
+                        return
+                    print "Opening Page %d" % index
+                    open_page = BeautifulSoup(urllib2.urlopen(current_page), 'lxml')
+                except:
+                    page_fails += 1
+                    print "Opening Page %d Fail %d" % (index, page_fails)
+                else:
+                    img_url = open_page.find('img', id='img')['src']
+                    path = os.path.join(title, "%02d" % index + os.path.splitext(img_url)[1])
+                    download_img(img_url, path, index)
+                    last_page = current_page
+                    current_page = open_page.find('div', id='i3').find('a')['href']
+                    break
+
 if __name__ == '__main__':
     hentai_url = sys.argv[1]
     if hentai_url is None:
@@ -23,21 +71,6 @@ if __name__ == '__main__':
     title = find_title(main_page)
     index = 0
     if title is not None:
-        last_page = None
-        current_page = main_page.find('div', class_='gdtm').find('a')['href']
-        while True:
-            index += 1
-            print "Downloading Page %d"%index
-            if (last_page == current_page):
-                break
-            else:
-                open_page = BeautifulSoup(urllib2.urlopen(current_page), 'lxml')
-                img_url = open_page.find('img', id='img')['src']
-                file = urllib2.urlopen(img_url)
-                path = os.path.join(title, "%02d" % index + os.path.splitext(img_url)[1])
-                with open(path, "wb") as code:
-                    code.write(file.read())
-                last_page = current_page
-                current_page = open_page.find('div', id='i3').find('a')['href']
+        open_page(main_page.find('div', class_='gdtm').find('a')['href'], title)
     else:
         exit()
